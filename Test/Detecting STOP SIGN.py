e9112e7dcca2869be067cffb6fd577ccd95eb106
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-07-15 12:39:27 -0400

    Update Detecting STOP SIGN.py

--- Test/Detecting STOP SIGN.py
+++ Test/Detecting STOP SIGN.py
@@ -1,24 +1,45 @@
+#import the neccessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera 
+import time
 import cv2
 
-def detect():
-    stop_cascade = cv2.CascadeClassifier("./cascades/stop_sign.xml")
-    camera = cv2.VideoCapture(0)
 
-    while(True):
-        ret, frame = camera.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        stop_signs = stop_cascade.detectMultiScale(gray,1.3,5)
-        for(x,y,w,h) in stop_signs:
-            img = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-            cv2.putText(img,"STOP SIGN W: " + str(w) + " H: " + str(h), (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 2)
-            print("Stop Sign Detected with Height of " + str(h) + " and width of " + str(w))
+#Initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size = (640, 480))
 
-        cv2.imshow("camera", frame)
-        if cv2.waitKey(83) & 0xff == ord("q"):
-            break
+#initialize the stop sign cascade
+stop_cascade = cv2.CascadeClassifier("./cascades/stop_sign.xml")
 
-    camera.release()
-    cv2.destroyAllWindows()
+#allow the camera to warmup
+time.sleep(0.1)
 
+#capture frames from the camera 
+for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
+	
+	#grab the raw numpy array representing the image, then initialize the timestamp
+	#and occupied/ unoccupied text
+	frame = frame.array
+	
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	stop_signs = stop_cascade.detectMultiScale(gray,1.3,5)
+	
+	for(x,y,w,h) in stop_signs:
+		img = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+		cv2.putText(img,"STOP SIGN W: " + str(w) + " H: " + str(h), (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 2)
+	
+	#show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) && 0xFF
+	
+	#clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	
+	#if the 'q' key was pressed, break from the loop
+	if(key == ord("q")):
+		break
 
-detect()
+cv2.destroyAllWindows()
